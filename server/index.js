6a009cfa8e21f2444f83b6b99a706571290a91a7
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-22 09:42:15 -0500

    safety push

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 
+// app.use(express.static(path.join(__dirname, "../pel.clone.html")));
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, "../pel.clone.html"))
 
